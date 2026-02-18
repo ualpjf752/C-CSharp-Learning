6605fa4743cc3e1f2b75ab7d71f0a27c53f2f853
PABLO JIMÉNEZ FERNÁNDEZ <pjf752@inlumine.ual.es>
2026-02-18 11:24:51 +0100

    add hello_world.c for C Learning

---
+++ C-CSharp-Learning-Path/C_Basics/hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h> // Librería estándar para entrada y salida
+
+int main() {
+    // printf es la función para mostrar texto en pantalla
+    printf("Hello World! This is my first C program.\n");
+    
+    // El 'return 0' le dice al sistema que el programa terminó bien
+    return 0;
+}
